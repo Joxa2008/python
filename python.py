d4c4aa9b5efdb152546fd9e7ad2845a3ebff8ca6
Joxa2008 <jovoooi2008@gmail.com>
2023-04-15 05:19:43 +0500

    first commit

---
+++ python.py
@@ -0,0 +1,40 @@
+import math 
+#16
+"""y = int(input("y is:"))
+
+x = math.cos(4*y**2)+7.151
+print(math.sqrt(x))"""
+
+#17
+"""y = int(input("y is:"))
+
+print(3*y**2+math.sqrt(y+1))"""
+
+#18
+"""y = int(input("y is:"))
+
+print(3*y**2+math.sqrt(y**3+1))
+"""
+
+#19
+"""y = int(input("y is:"))
+n = int(input("n is:"))
+g = int(input("g is:"))
+
+print(n*math.sqrt(y**3+1.09*g))"""
+
+#20
+"""y = int(input("y is:"))
+k = int(input("k is:"))
+x = int(input("x is:"))
+
+print(math.exp(k+y) + math.tan(x)*math.sqrt(y))
+"""
+#21
+"""
+y = int(input("y is:"))
+h = int(input("h is:"))
+
+print(math.exp(y+5.5)+9.1*h**3)"""
+
+
